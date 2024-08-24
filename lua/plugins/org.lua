93b3770101e03ef7b82d293ced75d2b7760f34b2
alen-mat <26516117+alen-mat@users.noreply.github.com>
2024-08-24 18:32:39 +0530

    note stuff

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,21 +1,129 @@
 return {
-  'nvim-orgmode/orgmode',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter'
-  },
-  ft = 'org',
-  config = function()
-    require('orgmode').setup_ts_grammar()
-    require('orgmode').setup {
-      org_agenda_files = { '~/Workspace/org/*', '~/my-orgs/**/*' },
-      org_default_notes_file = '~/Workspace/org/refile.org',
-      org_todo_keywords = { 'TODO(t)', 'NEXT', '|', 'DONE' },
-      mappings = {
-        global = {
-          org_agenda = { '<Leader>oa' },
-          org_capture = { '<Leader>oc' }
+  {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter'
+    },
+    ft = 'org',
+    config = function()
+      require('orgmode').setup_ts_grammar()
+      require('orgmode').setup {
+        org_agenda_files = { '~/Workspace/org/*', '~/my-orgs/**/*' },
+        org_default_notes_file = '~/Workspace/org/refile.org',
+        org_todo_keywords = { 'TODO(t)', 'NEXT', '|', 'DONE' },
+        mappings = {
+          global = {
+            org_agenda = { '<Leader>oa' },
+            org_capture = { '<Leader>oc' }
+          }
         }
       }
-    }
-  end
+    end
+  },
+  {
+    "nvim-neorg/neorg",
+    lazy = true,   -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {
+            config = {                -- We added a `config` table!
+              icon_preset = "varied", -- And we set our option here.
+            },
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        }
+      })
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    --ft = "markdown",
+    event = {
+      "BufReadPre " .. vim.fn.expand("~") .. "/workspace/sturdy-doodle/**.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "/workspace/sturdy-doodle/**.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter"
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Scooby-Dooby-Dooooo",
+          path = "~/workspace/sturdy-doodle",
+        },
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        }
+      },
+
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      ui = {
+        enable = true,
+        update_debounce = 200,
+        max_file_length = 5000,
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["!"] = { char = "", hl_group = "ObsidianImportant" },
+
+        },
+        bullets = { char = "•", hl_group = "ObsidianBullet" },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+        block_ids = { hl_group = "ObsidianBlockID" },
+        hl_groups = {
+          ObsidianTodo = { bold = true, fg = "#f78c6c" },
+          ObsidianDone = { bold = true, fg = "#89ddff" },
+          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+          ObsidianTilde = { bold = true, fg = "#ff5370" },
+          ObsidianImportant = { bold = true, fg = "#d73128" },
+          ObsidianBullet = { bold = true, fg = "#89ddff" },
+          ObsidianRefText = { underline = true, fg = "#c792ea" },
+          ObsidianExtLinkIcon = { fg = "#c792ea" },
+          ObsidianTag = { italic = true, fg = "#89ddff" },
+          ObsidianBlockID = { italic = true, fg = "#89ddff" },
+          ObsidianHighlightText = { bg = "#75662e" },
+        },
+      },
+    },
+  }
 }
+
+-- vim: ts=2 sts=2 sw=2 et
