c314172c6753d9a6726611b1148fd6f5bf593e65
alen-mat <26516117+alen-mat@users.noreply.github.com>
2024-08-25 13:49:41 +0530

    nvim spellcheck-stuff

--- plugin/custom-autocmd.lua
+++ plugin/custom-autocmd.lua
@@ -1,34 +1,43 @@
 -- Automatically reload the file if it is changed outside of Nvim, see https://unix.stackexchange.com/a/383044/221410.
--- It seems that `checktime` does not work in command line. We need to check if we are in command
--- line before executing this command, see also https://vi.stackexchange.com/a/20397/15292 .
-local api = vim.api
+-- It seems that `checktime` does not work in command line. We need to check if we are in command line before executing this command, 
+-- see also https://vi.stackexchange.com/a/20397/15292 .
 
-api.nvim_create_augroup("auto_read", { clear = true })
-
-api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
   pattern = "*",
-  group = "auto_read",
+  group = vim.api.nvim_create_augroup("auto_read", { clear = true }),
   callback = function()
     vim.notify("File changed on disk. Buffer reloaded!", vim.log.levels.WARN, { title = "nvim-config" })
   end,
 })
 
 -- Resize all windows when we resize the terminal
-api.nvim_create_autocmd("VimResized", {
-  group = api.nvim_create_augroup("win_autoresize", { clear = true }),
+vim.api.nvim_create_autocmd("VimResized", {
+  group = vim.api.nvim_create_augroup("win_autoresize", { clear = true }),
   desc = "autoresize windows on resizing operation",
   command = "wincmd =",
 })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
-api.nvim_create_autocmd('TextYankPost', {
+vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }) ,
   pattern = '*',
 })
 
+
+vim.api.nvim_create_autocmd({'BufAdd'}, {
+  group = vim.api.nvim_create_augroup('spell-check', { clear = true }),
+  callback = function(event)
+    vim.api.nvim_buf_create_user_command(event.buf, 'Ssc', function(_)
+      vim.opt_local.spelllang = 'en_us'
+      vim.opt_local.spell = true
+    end, { desc = '[S]tart [S]pell [C]heck' })
+    vim.api.nvim_buf_create_user_command(event.buf, 'Scs', function(_)
+      vim.opt_local.spelllang = ''
+      vim.opt_local.spell = false
+    end, { desc = '[S]pell [C]heck [S]top' })
+  end,
+})
