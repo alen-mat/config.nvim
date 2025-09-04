a7aced675525e2c33817fb895f6d63f0f382d9b8
alen-mat <26516117+alen-mat@users.noreply.github.com>
2025-09-04 21:27:29 +0530

    incline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,7 +5,7 @@ local clients_lsp_name = function()
   if client == nil then
     return '👀'
   else
-    return    ' '
+    return ' '
   end
 end
 
@@ -136,14 +136,42 @@ add_right3('location')
 return {
   {
     'nvim-lualine/lualine.nvim',
+    event = "VeryLazy",
     opts = opts
   },
   {
     'b0o/incline.nvim',
-    config = function()
-      require('incline').setup()
-    end,
-    -- Optional: Lazy load Incline
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = 'VeryLazy',
+    config = function()
+      local helpers = require 'incline.helpers'
+      local devicons = require 'nvim-web-devicons'
+      require('incline').setup {
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+          if filename == '' then
+            filename = '[No Name]'
+          end
+          local modified = vim.bo[props.buf].modified
+          if modified then
+            filename = filename .. '[+]'
+          end
+
+          return {
+            ' ',
+            ft_icon and { ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+            ' ',
+            { filename, gui = modified and 'bold,italic' or 'bold' },
+            ' ',
+          }
+        end,
+      }
+    end,
   },
 }
