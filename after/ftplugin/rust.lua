0bef8f3492e8e0608c528f064f6b224b27b76d4b
alen-mat <26516117+alen-mat@users.noreply.github.com>
2026-01-10 13:36:57 +0530

    ft rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -5,6 +5,7 @@ local utils = require("My.utils")
 vim.keymap.set('n', '<F10>', function()
   local client = utils.clients_lsp()
   if client then
-    utils.out_in_pp("cargo", {"build"}, client.config.root_dir)
+    local build_args = vim.g.rust_build_args or {"build"}
+    utils.out_in_pp("cargo", build_args, client.config.root_dir)
   end
 end, { desc = 'Build Project', silent = true })
