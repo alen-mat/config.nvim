756a31d78b871b8f6e913db276b80755f2a571d7
alen-mat <26516117+alen-mat@users.noreply.github.com>
2026-01-26 22:59:23 +0530

    neovide conf

--- plugin/options.lua
+++ plugin/options.lua
@@ -59,11 +59,11 @@ if vim.g.neovide then
   end
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_opacity = 0.9
+  vim.g.transparency = 0.9
   local alpha = function()
-    return string.format("%x", math.floor((255 * vim.g.transparency) or 0.4))
+    return string.format("%x", math.floor(255 * vim.g.transparency) )
   end
-  vim.g.neovide_opacity = 0.8
-  vim.g.transparency = 0.8
   vim.g.neovide_background_color = "#0f1117" .. alpha()
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
