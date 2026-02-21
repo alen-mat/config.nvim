cbba73a2558bfb62ff0f8f982e2de3f7f45f226a
alen-mat <26516117+alen-mat@users.noreply.github.com>
2026-02-22 00:23:42 +0530

    py lsp

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -1,4 +1,4 @@
-return {
+local base_config = {
   cmd = { 'pylsp' },
   filetypes = { 'python' },
   root_markers = {
@@ -9,5 +9,19 @@ return {
     'Pipfile',
   },
   single_file_support = true,
+  settings = {
+    pylsp = {
+      plugins = {}
+    }
+  }
 }
+local plugin_config = {
+  mypy = { enabled = true, report_progress = true, live_mode = true, }
+}
+for pn, pc in pairs(plugin_config) do
+  if vim.fn.exepath('pyflakes') then
+    base_config.settings.pylsp.plugins[pn] = pc
+  end
+end
+return base_config
 -- vim: ts=2 sts=2 sw=2 et
