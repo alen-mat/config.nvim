5ebe64982d04616f071d9d8e35723e197303b380
alen-mat <26516117+alen-mat@users.noreply.github.com>
2026-02-21 16:36:29 +0530

    housekeeping

---
+++ plugin/lsp.lua
@@ -0,0 +1,11 @@
+if vim.opt.diff:get() then
+  vim.notify("In diff-mode skipping lsp", vim.log.levels.WARN, { title = "nvim-lsp" })
+  return
+end
+
+if vim.fn.exepath('mise') then
+  require('mise').run()
+  require('lsp-setup')
+end
+
+-- vim: ts=2 sts=2 sw=2 et
